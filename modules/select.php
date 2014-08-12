95a06ff1cfbe2cedb70bcb267ee0c7e1bb069192
Scott Grodberg <scott@trwebco.com>
2014-08-12 17:48:51 -0400

    Hardcoded control widths to be css width:100%. This kills cf7 flexibility but it's ok since this will be used in wp widgets where the width is constained by the sidebar

--- modules/select.php
+++ modules/select.php
@@ -30,7 +30,8 @@ function wpcf7_select_shortcode_handler( $tag ) {
 	$atts['class'] = $tag->get_class_option( $class );
 	$atts['id'] = $tag->get_id_option();
 	$atts['tabindex'] = $tag->get_option( 'tabindex', 'int', true );
-
+  $atts['style'] = "width:100%;";
+	
 	if ( $tag->is_required() )
 		$atts['aria-required'] = 'true';
 
--- modules/text.php
+++ modules/text.php
@@ -35,11 +35,12 @@ function wpcf7_text_shortcode_handler( $tag ) {
 
 	$atts = array();
 
-	$atts['size'] = $tag->get_size_option( '40' );
+	$atts['style'] = "width:100%;";
 	$atts['maxlength'] = $tag->get_maxlength_option();
 	$atts['class'] = $tag->get_class_option( $class );
 	$atts['id'] = $tag->get_id_option();
 	$atts['tabindex'] = $tag->get_option( 'tabindex', 'int', true );
+  
 
 	if ( $tag->has_option( 'readonly' ) )
 		$atts['readonly'] = 'readonly';
